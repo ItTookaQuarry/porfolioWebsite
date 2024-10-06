8d2ae2a7deddb111f78aaebdf8f7e1c6e1d58e2d
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2024-10-06 20:24:03 +0200

    new commit

--- components/EcommerceProject.jsx
+++ components/EcommerceProject.jsx
@@ -32,8 +32,10 @@ export default function EcommerceProject() {
 
 <div className="grid col-span-full mds:col-span-2 gap-y-2 mds:gap-y-4 row-auto mds:row-start-2  font-vt323 text-2xl font-bold bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent">
 <h1 className='text-center hidden mds:block'>{language== 'pl' ? "Sklep z edytorem graficznym": "Store with a graphic editor"}</h1>
-{appDescription[language].map((e)=>{
-    return (<div className='text-base md:text-md px-10'>{e}</div>)
+{appDescription[language].map((e,i)=>{
+    return (<div 
+      key={i}
+      className='text-base md:text-md px-10'>{e}</div>)
 })}
 
 <Button
@@ -52,7 +54,17 @@ export default function EcommerceProject() {
 <div className='m-auto row-start-3 md:row-start-2 col-span-full mds:col-start-3 mds:row-start-2 mds:col-span-2 m-auto font-vt323 text-2xl font-bold bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent'>
 <h1 className='text-center block mds:hidden'>{language== 'pl' ? "Sklep z edytorem graficznym": "Store with a graphic editor"}</h1>
 <video controls class="border-4 m-auto border-purple-500 rounded-lg shadow-lg  h-[200px] w-[300px] mds:h-[400px] mds:w-[500px]">
+
+  
+<track
+            src="/path/to/captions.vtt"
+            kind="subtitles"
+            srcLang="en"
+            label="English"
+          />
   <source
+
+  
             src="https://firebasestorage.googleapis.com/v0/b/stripesetup-7f02d.appspot.com/o/sklep.mp4?alt=media&token=77fe0a21-bb27-4139-9bb7-4dc8fdbd964d"
             type="video/mp4"
           />
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -72,7 +72,7 @@ export default function Footer() {
 
             </Link>
             <Link
-              class="mb-4 flex items-center justify-center font-semibold  md:justify-center"
+              className="mb-4 flex items-center justify-center font-semibold  md:justify-center"
            href={"/"}
             >
                 {language == 'pl' ? "Moje projekty" : "My Projects"}
@@ -87,36 +87,36 @@ export default function Footer() {
 
           <div className="tex-white">
        
-            <p class="mb-4 flex items-center justify-center ">
+            <p className="mb-4 flex items-center justify-center ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="mr-3 h-5 w-5"
+                className="mr-3 h-5 w-5"
               >
                 <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
                 <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
               </svg>
               Dąbrowa Górnicza
             </p>
-            <p class="mb-4 flex items-center justify-center ">
+            <p className="mb-4 flex items-center justify-center ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="mr-3 h-5 w-5"
+                className="mr-3 h-5 w-5"
               >
                 <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                 <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
               </svg>
               pkncz089@gmail.com
             </p>
-            <p class="mb-4 flex items-center justify-center ">
+            <p className="mb-4 flex items-center justify-center ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="mr-3 h-5 w-5"
+                className="mr-3 h-5 w-5"
               >
                 <path
                   fill-rule="evenodd"
@@ -130,7 +130,7 @@ export default function Footer() {
         </div>
       </div>
 
-      {<div class="bg-neutral-200 p-6 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 text-white" style={{backgroundColor:"black"}}>
+      {<div className="bg-neutral-200 p-6 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 text-white" style={{backgroundColor:"black"}}>
         <span>© 2024</span>
 
       </div> }
